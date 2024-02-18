7c1b80dd733e1d17c0e77af4952e9180da1ef246
Florian Kainrath <flo.kainrath@gmail.com>
2024-02-18 17:09:50 +0100

    removed kotlin classes

--- src/main/kotlin/org/fotoparty/fotopartybackend/persistence/SessionRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.fotoparty.fotopartybackend.persistence
-
-import org.fotoparty.fotopartybackend.persistence.entity.GameSession
-import org.springframework.data.mongodb.repository.MongoRepository
-
-
-interface SessionRepository : MongoRepository<GameSession, Int> {
-}
--- src/main/kotlin/org/fotoparty/fotopartybackend/persistence/entity/GameSession.kt
+++
@@ -1,9 +0,0 @@
-package org.fotoparty.fotopartybackend.persistence.entity
-
-import org.springframework.data.annotation.Id
-
-
-data class GameSession(
-    @Id
-    val gameCode: Int,
-)
